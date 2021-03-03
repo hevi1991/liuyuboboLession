37f8a560d198482643f9bc84ebbeb20ce46b0edd
hevi1991 <206696343@qq.com>
2021-03-03 18:19:22 +0800

    Initial commit

---
+++ LinearSearch/src/LinearSearch.java
@@ -0,0 +1,32 @@
+/**
+ * 线性查找法
+ */
+
+
+public class LinearSearch {
+
+    // 将构造函数设置为私有，让外部无法实例化
+    private LinearSearch() {
+    }
+
+    public static int search(int[] data, int target) {
+
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] data = {1, 23, 4, 44, 5, 6, 123};
+        int res = LinearSearch.search(data, 44);
+        System.out.println(res);
+
+        int res2 = LinearSearch.search(data, 1234);
+        System.out.println(res2);
+    }
+
+}

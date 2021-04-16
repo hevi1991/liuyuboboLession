bcdc73814060a1e43fb9fc35b850b712d4997cdc
hevi1991 <206696343@qq.com>
2021-04-16 11:04:39 +0800

    阶段一 动态数据结构基础：链表 1-6 使用链表实现栈

---
+++ Stack/src/LinkedListStack.java
@@ -0,0 +1,56 @@
+/**
+ * 使用链表实现栈结构
+ *
+ * @param <E> 元素类型
+ */
+public class LinkedListStack<E> implements Stack<E> {
+
+    private LinkedList<E> list;// LinkedList模块项目
+
+    public LinkedListStack() {
+        list = new LinkedList<E>();
+    }
+
+    @Override
+    public int getSize() {
+        return list.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public void push(E e) {
+        list.addFirst(e);// 链表取头节点的复杂度是O(1)
+    }
+
+    @Override
+    public E pop() {
+        return list.removeFirst();
+    }
+
+    @Override
+    public E peek() {
+        return list.getFirst();
+    }
+
+    @Override
+    public String toString() {
+        return "Stack: top " + list;
+    }
+
+    public static void main(String[] args) {
+
+        LinkedListStack<Integer> stack = new LinkedListStack<>();
+
+        for (int i = 0; i < 5; i++) {
+            stack.push(i);
+            System.out.println(stack);
+        }
+
+        stack.pop();
+        System.out.println(stack);
+    }
+}
